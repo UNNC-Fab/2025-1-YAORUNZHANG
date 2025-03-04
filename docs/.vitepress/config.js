5800b265234b312f888b693548ae62f07109e858
Patton-Zhang <yaorun_zhang@yahoo.com>
2025-03-04 19:05:08 +0800

    update

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vitepress'
 
 export default defineConfig({
+  base: '/2025-1-YAORUNZHANG/',
   title: "UNNC-FAB-2025",
   description: "UNNC-FAB-2025 Documentation",
   themeConfig: {
